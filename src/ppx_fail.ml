0018e06d9d2f04cf4d6b8fded2d5c48d3a733336
Jeremie Dimino <jdimino@janestreet.com>
2015-09-24 19:48:42 +0100

    113.09.00

---
+++ src/ppx_fail.ml
@@ -0,0 +1,25 @@
+open! StdLabels
+open Ppx_core.Std
+open Parsetree
+open Ast_builder.Default
+
+[@@@metaloc loc]
+
+let map = object
+  inherit Ast_traverse.map as super
+
+  method! expression e =
+    match e.pexp_desc with
+    | Pexp_ident { txt = Lident "failwiths"; _ } ->
+      let loc = e.pexp_loc in
+      pexp_apply e ~loc [("here", Ppx_here_expander.lift_position ~loc
+                                    loc.Location.loc_start)]
+    | _ -> super#expression e
+end
+
+let () =
+  Ppx_driver.register_code_transformation
+    ~name:"fail"
+    ~impl:map#structure
+    ~intf:map#signature
+;;
