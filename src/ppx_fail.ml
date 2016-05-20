f79d8bd0a0d08f56d8fbed22afdbb4a4242abdf5
Jeremie Dimino <jdimino@janestreet.com>
2016-05-20 12:01:10 +0100

    113.43+70

--- src/ppx_fail.ml
+++ src/ppx_fail.ml
@@ -1,23 +1,20 @@
 open! StdLabels
-open Ppx_core.Std
-open Parsetree
-open Ast_builder.Default
+open! Ppx_core.Std
 
-[@@@metaloc loc]
-
-let map = object
-  inherit Ast_traverse.map as super
-
-  method! expression e =
-    match e.pexp_desc with
-    | Pexp_ident { txt = Lident "failwiths"; _ } ->
-      let loc = e.pexp_loc in
-      pexp_apply e ~loc [("here", Ppx_here_expander.lift_position ~loc)]
-    | _ -> super#expression e
-end
+let expand (e : Parsetree.expression) =
+  match e.pexp_desc with
+  | Pexp_apply (id, args) when not (List.exists args ~f:(fun (lab, _) -> lab = "here")) ->
+    let here = Ppx_here_expander.lift_position ~loc:id.pexp_loc in
+    Some { e with pexp_desc = Pexp_apply (id, ("here", here) :: args) }
+  | Pexp_ident _ ->
+    (* This case is a bit dubious but it's what was done before *)
+    let loc = e.pexp_loc in
+    let here = Ppx_here_expander.lift_position ~loc in
+    Some (Ast_builder.Default.pexp_apply ~loc e [("here", here)])
+  | _ -> None
+;;
 
 let () =
   Ppx_driver.register_transformation "fail"
-    ~impl:map#structure
-    ~intf:map#signature
+    ~rules:[ Context_free.Rule.special_function "failwiths" expand ]
 ;;
