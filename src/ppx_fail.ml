524e0da88182c5e98f1caa742e1451d93251db96
Jeremie Dimino <jdimino@janestreet.com>
2016-07-12 17:08:10 +0100

    114.01+04

--- src/ppx_fail.ml
+++ src/ppx_fail.ml
@@ -3,14 +3,15 @@ open! Ppx_core.Std
 
 let expand (e : Parsetree.expression) =
   match e.pexp_desc with
-  | Pexp_apply (id, args) when not (List.exists args ~f:(fun (lab, _) -> lab = "here")) ->
+  | Pexp_apply (id, args) when
+      not (List.exists args ~f:(fun (lab, _) -> lab = Asttypes.Labelled "here")) ->
     let here = Ppx_here_expander.lift_position ~loc:id.pexp_loc in
-    Some { e with pexp_desc = Pexp_apply (id, ("here", here) :: args) }
+    Some { e with pexp_desc = Pexp_apply (id, (Labelled "here", here) :: args) }
   | Pexp_ident _ ->
     (* This case is a bit dubious but it's what was done before *)
     let loc = e.pexp_loc in
     let here = Ppx_here_expander.lift_position ~loc in
-    Some (Ast_builder.Default.pexp_apply ~loc e [("here", here)])
+    Some (Ast_builder.Default.pexp_apply ~loc e [(Labelled "here", here)])
   | _ -> None
 ;;
 
