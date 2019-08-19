f3d8d86404a5bd964c587c1e0cad006dbf3de15b
Xavier Clerc <xclerc@janestreet.com>
2019-08-19 12:55:18 +0100

    v0.13-preview.120.49+223

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,7 @@
-
-let { Lexing.pos_lnum = _; _} = [%here]  (* test that we've got pa_here loaded *)
+let { Lexing.pos_lnum = _; _ } = [%here] (* test that we've got pa_here loaded *)
 
 let () =
-  let failwiths ~here:{Lexing.pos_lnum = _; _} () = () in
+  let failwiths ~here:{ Lexing.pos_lnum = _; _ } () = () in
   (* compilation fails if this is not rewritten to failwithp with the right arguments *)
-  failwiths ();
+  failwiths ()
 ;;
