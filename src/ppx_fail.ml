1d08026afbb61e6b050fca0e9365d762cefad7c2
Xavier Clerc <xclerc@janestreet.com>
2020-01-08 16:33:16 +0000

    v0.14-preview.122.03+72

--- src/ppx_fail.ml
+++ src/ppx_fail.ml
@@ -9,12 +9,14 @@ let expand (e : Parsetree.expression) =
               match lab with
               | Labelled "here" -> true
               | _ -> false)) ->
-    let here = Ppx_here_expander.lift_position ~loc:id.pexp_loc in
+    let here =
+      Ppx_here_expander.lift_position ~loc:id.pexp_loc |> Merlin_helpers.hide_expression
+    in
     Some { e with pexp_desc = Pexp_apply (id, (Labelled "here", here) :: args) }
   | Pexp_ident _ ->
     (* This case is a bit dubious but it's what was done before *)
     let loc = e.pexp_loc in
-    let here = Ppx_here_expander.lift_position ~loc in
+    let here = Ppx_here_expander.lift_position ~loc |> Merlin_helpers.hide_expression in
     Some (Ast_builder.Default.pexp_apply ~loc e [ Labelled "here", here ])
   | _ -> None
 ;;
